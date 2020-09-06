70456d8e4bd3fdaa5ba3db7232c3a1db011e4f09
Shahd Allemie <shahdallemie@Shahds-MacBook-Pro.local>
2020-09-06 14:25:28 +0300

    some css

--- pets/models.py
+++ pets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Pet(models.Model):
@@ -7,3 +8,6 @@ class Pet(models.Model):
     available = models.BooleanField(default = True,null=True,blank=True)
     image = models.ImageField(null=True,blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pet_id':self.id})
