82d4ef63cfe573280f99ece093fd2a4cb5f8c604
Shahd Allemie <shahdallemie@Shahds-MacBook-Pro.local>
2020-09-06 11:43:49 +0300

    created list view and html

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render.redirect
+from .models import Pet
 
 # Create your views here.
+def list(request):
+    pets = Pet.objects.filter(available=True)
+    context = {
+    "pets": pets,
+    }
+    return render(request, 'list.html', context)
