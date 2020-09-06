b7cb85ee47db32cd0ce1de6de6dd170a5c87f165
Heba <36724067+heba@users.noreply.github.com>
2020-09-06 11:21:24 +0300

    added a pet model

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    available = models.BooleanField(default = True)
+    image = models.ImageField()
+    price = models.DecimalField()
